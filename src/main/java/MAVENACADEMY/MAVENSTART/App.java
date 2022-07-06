468d282e33a727bc41be6eafe87877593cc74b6a
PallaviDua <pallavituteja13@gmail.com>
2022-07-06 07:32:39 +0530

    first commit

---
+++ src/main/java/MAVENACADEMY/MAVENSTART/App.java
@@ -0,0 +1,13 @@
+package MAVENACADEMY.MAVENSTART;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
